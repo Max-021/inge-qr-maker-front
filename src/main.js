720f57967d969276276f5593c2a2c193cb7efcf6
Maximiliano Frega <maxifrega21@gmail.com>
2023-06-26 19:08:40 -0300

    cambios en qr

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import store from './store/index'
 
 const app = createApp(App)
 
-let basePath = process.env.NODE_ENV === 'production' ? 'https://inge-qr-maker.vercel.app' : 'http://localhost:3001'
-let baseApiPath = process.env.NODE_ENV === 'production' ? 'https://inge-qr-maker.vercel.app/qr' : 'http://localhost:3001/qr'
+let basePath = process.env.NODE_ENV === 'production' ? 'https://ingeqrmaker-maxifrega21.b4a.run' : 'http://localhost:3001'
+let baseApiPath = process.env.NODE_ENV === 'production' ? 'https://ingeqrmaker-maxifrega21.b4a.run/qr' : 'http://localhost:3001/qr'
 app.provide('basePath', basePath)
 app.provide('apiUrl', baseApiPath)
 
